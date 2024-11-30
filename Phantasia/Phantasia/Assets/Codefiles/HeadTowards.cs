0f16a7b3d46e4909781dc8d6e63f549b27ce9e89
DebbieGao17 <debbiegzc@qq.com>
2024-11-30 15:00:26 +0800

    toon shader improvement face shading bug?

---
+++ Phantasia/Phantasia/Assets/Codefiles/HeadTowards.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+
+public class HeadTowards : MonoBehaviour
+{
+    public Material Facematerial;
+    // Start is called before the first frame update
+    private void SetHeadDirection()
+    {
+        if(this.Facematerial != null)
+        {
+            this.Facematerial.SetVector("_HeadForward",this.transform.forward);
+            this.Facematerial.SetVector("_HeadRight",this.transform.right);
+            Debug.Log("3");
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        this.SetHeadDirection();
+    }
+}
