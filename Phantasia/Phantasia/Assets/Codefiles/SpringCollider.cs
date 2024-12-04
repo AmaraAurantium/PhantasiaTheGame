a33c0539fb914c31e84c1d55cf7793096da50632
DebbieGao17 <debbiegzc@qq.com>
2024-12-04 18:23:26 +0800

    added physics

---
+++ Phantasia/Phantasia/Assets/Codefiles/SpringCollider.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class SpringCollider : MonoBehaviour
+{
+	//半径
+    public float radius = 0.5f;
+	public float disX = 0.0f;
+	public float disY = 0.0f;
+	public float disZ = 0.0f;
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere(transform.position + new Vector3(disX, disY, disZ), radius);
+	}
+}
