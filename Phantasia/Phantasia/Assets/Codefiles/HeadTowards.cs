463f0c9b2f811067d23668a1fcee4d80951bd56c
DebbieGao17 <debbiegzc@qq.com>
2024-12-04 09:53:15 +0800

    still unfixed????

--- Phantasia/Phantasia/Assets/Codefiles/HeadTowards.cs
+++ Phantasia/Phantasia/Assets/Codefiles/HeadTowards.cs
@@ -12,7 +12,7 @@ public class HeadTowards : MonoBehaviour
     // Start is called before the first frame update
     private void SetHeadDirection()
     {
-        Material[] materials = mesh.materials;
+        Material[] materials = mesh.sharedMaterials;
 
         materials[1].SetVector("_HeadForward", this.transform.forward);
         materials[1].SetVector("_HeadRight", this.transform.right);
